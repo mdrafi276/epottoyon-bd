2b13682c53eb5e3eea7c2dcaddba00237132b59c
mdrafi276 <mdrafi37473@gmail.com>
2024-02-29 17:42:46 +0600

    create banner section and create chetegory section

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Header from "../../components/Header/Header";
+// import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <>
-            <Header />
+            {/* <Header /> */}
             <Outlet />
             <Footer />
         </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
+import ChetegoryList from "../../components/Header/components/ChetegoryList/ChetegoryList";
+import Hero from "../../components/Header/components/Hero/Hero";
 import useMainContext from "../../hooks/useMainContext/useMainContext";
 
 const Home = () => {
-    return <div>homepage</div>;
+    return (
+    <div>
+        
+        <Hero/>
+        <ChetegoryList/>
+        </div>
+        )
 };
 
 export default Home;
