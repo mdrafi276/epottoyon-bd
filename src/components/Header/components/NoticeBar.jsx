e90adbcfe7edc23e91ebae1b77bd6b8be0cde7d5
EligibleDev <eligibledev@gmail.com>
2024-03-01 10:24:00 +0600

    folder structure fixed

--- src/components/Header/components/NoticeBar.jsx
+++ src/components/Header/components/NoticeBar.jsx
@@ -3,11 +3,11 @@ import Marquee from "react-fast-marquee";
 const NoticeBar = () => {
     return (
         <nav className="flex items-center gap-3 py-3 -4 xl:px-0">
-            <b className="text-2xl sm:text-3xl">নোটিশ:</b>
-            <Marquee className="text-xl sm:text-2xl" pauseOnHover>
-                <h3>আমার প্রথম বাংলা শিরোনাম</h3>
-                <h3>বাংলা ভাষায় একটি মহান শিরোনাম</h3>
-                <h3>বাংলা ভাষায় একটি শিল্প</h3>
+            <b className="text-2xl sm:text-2xl">নোটিশ:</b>
+            <Marquee className="text-xl sm:text-xl" pauseOnHover>
+                <h3 className="pl-6">আমার প্রথম বাংলা শিরোনাম</h3>
+                <h3 className="pl-6">বাংলা ভাষায় একটি মহান শিরোনাম</h3>
+                <h3 className="pl-6">বাংলা ভাষায় একটি শিল্প</h3>
             </Marquee>
         </nav>
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
+import ChetegoryList from "./components/ChetegoryList/ChetegoryList";
+import Hero from "./components/Hero/Hero";
 
 const Home = () => {
     return (
-    <div>
-        
-        <Hero/>
-        <ChetegoryList/>
+        <div>
+            <Hero />
+            <ChetegoryList />
         </div>
-        )
+    );
 };
 
 export default Home;
