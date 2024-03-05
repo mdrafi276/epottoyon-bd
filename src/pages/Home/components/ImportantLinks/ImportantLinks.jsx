6d3d85972e5ad0ce5d1d2c26de657538016f6168
mdrafi276 <mdrafi37473@gmail.com>
2024-03-06 00:24:12 +0600

    home page and registation page design added

--- src/pages/Home/components/ImportantLinks/ImportantLinks.jsx
+++ src/pages/Home/components/ImportantLinks/ImportantLinks.jsx
@@ -6,20 +6,26 @@ const ImportantLinks = () => {
     const { importantLinks } = useMainContext();
 
     return (
-        <div className="w-full lg:w-1/3">
-            <div className="w-full border-l-4 border-l-[#8a569b] shadow-md py-6 px-3 text-xl text-[#8a569b] font-bold">
-                গুরুত্বপূর্ণ লিংক
-            </div>
-
-            <div className="h-96 overflow-y-scroll flex flex-col gap-2 py-6">
-                {importantLinks?.map((item) => (
-                    <Link className="flex items-center gap-3" to={item?.link} key={item?.id}>
-                        <FaLink className="text-[#8a569b] text-2xl " />
-                        <p className="text-xl font-semibold hover:text-[#8a569b] transition-colors">{item?.label}</p>
-                    </Link>
-                ))}
-            </div>
+      <div className="w-full lg:w-1/3 border-2 border-black ">
+        <div className="w-full  text-center  shadow-md py-6 px-3 text-xl text-white bg-[#8a569b] font-bold">
+          গুরুত্বপূর্ণ লিংক
         </div>
+
+        <div className="h-96 overflow-y-scroll flex  p-2 flex-col gap-2 py-6">
+          {importantLinks?.map((item) => (
+            <Link
+              className="flex items-center gap-3"
+              to={item?.link}
+              key={item?.id}
+            >
+              <FaLink className="text-[#8a569b] text-2xl " />
+              <p className="text-xl font-semibold hover:text-[#8a569b] transition-colors">
+                {item?.label}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </div>
     );
 };
 
