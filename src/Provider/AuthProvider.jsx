b5d835002492926a4bb135882e0a9ea9f3de846c
EligibleDev <eligibledev@gmail.com>
2024-03-05 16:10:27 +0600

    console error fixed

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,49 +1,51 @@
 import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, } from "firebase/auth"
-import axios from "axios";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
 
 const AuthContext = createContext(null);
-const auth = getAuth(app)
-const useAxois = axios()
+const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
-const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  const createUser = (email, password) => {
-    setLoading(true);
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
-  const signIn = (email, password) => {
-    setLoading(true);
-    return signInWithEmailAndPassword(auth, email, password);
-  };
- 
-  const logOut = () => {
-    setLoading(true);
-    return signOut(auth);
-  };
-  useEffect(() => {
-    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      console.log("use in the auth state  change", currentUser);
-      setLoading(false);
-    });
-    return () => unSubscribe();
-  }, []);
-  const authInfo = {
-    user,
-    loading,
-    createUser,
-    logOut,
-    signIn,
-   
-  };
-  return (
-    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-  );
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            console.log("use in the auth state change", currentUser);
+            setLoading(false);
+        });
+        return () => unSubscribe();
+    }, []);
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        logOut,
+        signIn,
+    };
+    return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
