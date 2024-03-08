a501815aa3dada30d29846fa7a66656509cfb942
mdrafi276 <mdrafi37473@gmail.com>
2024-03-08 23:02:25 +0600

    edit

--- src/routers/MainRouter/MainRouter.jsx
+++ src/routers/MainRouter/MainRouter.jsx
@@ -3,7 +3,6 @@ import MainLayout from "../../layouts/MainLayout/MainLayout";
 import Home from "../../pages/Home/Home";
 import Register from "../../pages/Register/Register";
 import LoyalRegister from "../../pages/Register/LoyalRegister";
-import Dashboard from "../../Dashboard/Dashboard";
 
 const MainRouter = createBrowserRouter([
     {
@@ -22,15 +21,7 @@ const MainRouter = createBrowserRouter([
                 path:"/loRegister",
                 element:<LoyalRegister/>
             },
-            {
-                path:"/dashboard",
-                element:<Dashboard/>,
-                children:[
-                    {
-                        path:""
-                    }
-                ]
-            }
+            
         ],
     },
 ]);
